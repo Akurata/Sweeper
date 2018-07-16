459a723c8c3f1cce4e0a7c0e4c5f2425ec9882b3
Alexander Y Kurata <Alexander.Kurata1@marist.edu>
2018-07-16 17:03:34 -0400

    Refacotring + Url extrator

--- index.js
+++ index.js
@@ -7,18 +7,13 @@ var bodyParser = require('body-parser');
 var fetch = require('node-fetch');
 var request = require('request');
 var async = require('async')
-
 var zlib = require('zlib');
-//const {gzip, ungzip} = require('node-gzip');
 
 var response;
 var count = [];
-var pages = {};
-
-var full;
-var test = "";
 
 var tree = "";
+var list = [];
 
 app.use(bodyParser.urlencoded({extended: true}));
 // doAsGroupId=20182&controlPanelCategory=current_site.pages&p_p_id=156
@@ -39,15 +34,12 @@ request('https://www.marist.edu/group/control_panel/manage?doAsGroupId=20182&con
   if(res.headers['content-encoding'] == 'gzip') {
     zlib.gunzip(res.body, (err, dezip) => {
       console.log("Res recieved");
-
       if (err) console.log(err);
-      //console.log(dezip.toString());
+
       response = dezip.toString();
-      full = response;
       response = response.substring(response.indexOf("children:b.formatJSONResults") + 29, response.indexOf("),cssClasses:{pages:p"));
 
       var temp = response.split('');
-
       var startIndex = 0;
       var i = -1;
       do {
@@ -74,15 +66,10 @@ request('https://www.marist.edu/group/control_panel/manage?doAsGroupId=20182&con
           startIndex = i; //+1
         }
       }while (temp[i] != undefined);
-      pages = temp //JSON.parse(temp.join(''));
-
-
 
       tree = JSON.parse(tree);
       fillBranch(tree.layouts);
     });
-  } else {
-    //console.log(body)
   }
 });
 
@@ -116,65 +103,45 @@ function fillBranch(layouts) {
               var branch = JSON.parse(dezip.toString())
 
               layouts[i].children = branch;
-              console.log(branch)
+              //console.log(branch)
 
 
               if(branch.layouts.length != 0) {
                 fillBranch(branch.layouts);
               }
             }
-
-
-
-
             callback();
           });
         }
       });
     }
+  }, () => {
+    fs.writeFile('./tree.json', JSON.stringify(tree, 'utf8', 4), () => {
+      console.log("Done!");
+    });
   });
-/*
-
-  */
 }
 
 
-var parent = 3;
-var selPlid = 0;
-request('https://www.marist.edu/c/layouts_admin/get_layouts?cmd=get&doAsGroupId=20182&end=20&groupId=20182&incomplete=1&limit=20&p_auth=qVxIXONA&p_l_id=20176&p_p_id=88&parentLayoutId=' + parent + '&privateLayout=0&selPlid=' + selPlid + '&start=0&treeId=layoutsTree', {
-  encoding: null,
-  credentials: 'same-origin',
-  headers: {
-    'Host': 'www.marist.edu',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64; rv:61.0) Gecko/20100101 Firefox/61.0',
-    'Accept': '*/*',
-    'Accept-Language': 'en-US,en;q=0.5',
-    'Accept-Encoding': 'gzip, deflate, br',
-    'Referer': 'https://www.marist.edu/group/control_panel?refererPlid=20185&doAsGroupId=20182&controlPanelCategory=current_site.pages&p_p_id=156&_156_selPlid=137837',
-    'X-Requested-With': 'XMLHttpRequest',
-    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-    'Content-Length': '224',
-    'Cookie': '__utma=220728263.1974296480.1521576471.1526050319.1528481507.5; __utmz=220728263.1521576471.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); COOKIE_SUPPORT=true; GUEST_LANGUAGE_ID=en_US; _ga=GA1.2.1974296480.1521576471; JSESSIONID=A0F3F5D9A128270594492EBC279B4538; _gid=GA1.2.1139722017.1531746938; LFR_SESSION_STATE_20159=1531752816621; COMPANY_ID=20155; ID=6168316a6c526452776a4a515133627450776e5930773d3d; USER_UUID=374652383144446d48454f78374c4f4a49384335425073376b58743249394d456e613965485362325475303d; LFR_SESSION_STATE_175775=expired; LOGIN=616b7572617461; PASSWORD=594c424174496b5668376f5632393558486f324761413d3d; REMEMBER_ME=true; SCREEN_NAME=45344a4a744d4f30464e6d543856794e4b4e6d6672513d3d',
-    'Connection': 'keep-alive'
-  },
-  method: 'POST'
-}, (err, res, body) => {
-  if(res.headers['content-encoding'] == 'gzip') {
-  zlib.gunzip(res.body, (err, dezip) => {
-    if(err) console.log(err);
-    //console.log(dezip.toString());
-    test += dezip.toString();
+function writeList(a) {
+  async.forEachOf(a, (result, i, callback) => {
+    list.push(a[i].friendlyURL);
+
+    if(a[i].hasChildren) {
+      if(a[i].children.layouts.length != 0) {
+        writeList(a[i].children.layouts);
+      }else {
+        callback();
+      }
+    }
+  }, (err) => {
+    if(err) console.error(err);
+    //console.log(list)
+    fs.writeFile('./pageList.txt', list, () => {
+      console.log("List Done!");
+    })
   });
-  }
-});
-
-
-
-
-
-
-
-
+}
 
 
 
@@ -183,13 +150,12 @@ app.get('/', (req, res) => {
   //res.json(tree.split(''))
   res.end();
 });
-app.get('/full', (req, res) => {
-  res.send(full)
-});
-app.get('/test', (req, res) => {
-  res.json(JSON.parse(test));
+app.get('/list', (req, res) => {
+  writeList(tree.layouts);
+  res.send(list)
   res.end();
-})
+});
+
 
 app.listen('3000', () => {
   console.log("Start");
